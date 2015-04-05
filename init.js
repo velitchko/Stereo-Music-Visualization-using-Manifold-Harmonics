444cb30396b60c19e80de212a30ce5839e75d5f4
Velitchko Filipov <velitchko@crocotta.co.uk>
2015-04-05 16:04:28 +0200

    Object Loader, Audio Loader basic functionality

---
+++ init.js
@@ -0,0 +1,99 @@
+var canvas_w = 1280;
+var canvas_h = 720;
+var canvas;
+var renderer;
+var camera;
+var controls;
+var scene;
+var sphere;
+
+var ratio = (canvas_w / canvas_h);
+var ambient;
+var directionalLight;
+var loader;
+var manager;
+
+function init()
+{
+	initAudio();
+	
+	canvas = document.getElementById("canvas");
+	renderer = new THREE.WebGLRenderer({ clearAlpha: 1, antialias: true } );
+
+	canvas.appendChild(renderer.domElement);
+
+
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera(40, ratio, 0.1, 1000);
+	setCamera(10,10,100);
+	setLight();
+	scene.add(camera);
+	
+
+	
+	manager = new THREE.LoadingManager();
+	manager.onProgress  = function(item, loaded, total)
+	{
+		console.log("Model loaded:" + item, loaded, total);
+	}
+	loader = new THREE.OBJLoader(manager);
+	setLoader();
+
+	controls = new THREE.TrackballControls(camera, renderer.domElement);
+	setControls();
+
+	renderer.setSize(canvas_w, canvas_h);
+	console.log('WebGL initialized.')
+	animate();
+}
+
+function setLight()
+{
+	ambient = new THREE.AmbientLight(0xffffff);
+	directionalLight = new THREE.DirectionalLight(0xffffff);
+    directionalLight.position.set(1, 1, 1).normalize();
+    scene.add(ambient);
+    scene.add(directionalLight);
+}
+
+function setLoader()
+{
+	
+	loader.load('models/male.obj', function ( object ) 
+	{
+		object.position.x = 0;
+		object.position.y = 0;
+		object.position.z = 0;
+  		//object.scale.set(0.2,0.2,);
+		scene.add(object);
+	}
+	);
+	
+}
+
+function setCamera(x,y,z)
+{
+	camera.position.set(x,y,z);
+}
+
+
+function setControls()
+{
+	controls.rotateSpeed = 5.0;
+	controls.zoomSpeed = 5;
+	controls.panSpeed = 2;
+	controls.noZoom = false;
+	controls.noPan = false;
+	controls.staticMoving = true;
+	controls.dynamicDampingFactor = 0.3;
+}
+
+
+function animate()
+{
+	requestAnimationFrame(animate);
+	controls.update();
+	camera.lookAt(scene.position);
+	renderer.render(scene,camera);
+
+}
---
+++ player.js
@@ -0,0 +1,56 @@
+var context;
+var analyzer;
+var processor;
+var source;
+
+var bufferLoader;
+
+
+function initAudio()
+{
+	try
+	{
+		// Fix up for prefixing
+		window.AudioContext = window.AudioContext || window.webkitAudioContext;
+		context = new AudioContext();
+
+		bufferLoader = new BufferLoader(context,
+		[
+		'music/queen.mp3'
+		],
+		finishedLoading
+		);
+		bufferLoader.load();
+
+	}
+	catch(e)
+	{
+		alert('Web Audio API is not supported in this browser');
+		console.log(e);
+
+	}
+	console.log("Audio initialized.")
+}
+
+function finishedLoading(bufferList)
+{
+	source = context.createBufferSource();
+	source.buffer = bufferList[0];
+	source.connect(context.destination);	
+	$("#play").attr("disabled", false);
+	console.log("Song: " + bufferLoader.urlList[0] + " loaded.")
+}
+
+function play()
+{
+	$("#play").attr("disabled", true);
+	$("#stop").attr("disabled", false);
+	source.start(0);
+}
+
+function stop()
+{
+	$("#play").attr("disabled", false);
+	$("#stop").attr("disabled", true);
+	source.stop();
+}
