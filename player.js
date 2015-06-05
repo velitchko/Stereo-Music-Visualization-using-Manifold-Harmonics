b19ec81462736dde5725ca7321387c181d7da7ce
Velitchko Filipov <velitchko@crocotta.co.uk>
2015-06-05 16:39:24 +0200

    Fixed some issues with the audio player getByteFrequencyData now works

--- player.js
+++ player.js
@@ -2,52 +2,85 @@ var context;
 var analyzer;
 var processor;
 var source;
-
+var buffer;
 var bufferLoader;
 var freq = 512;
 var freq_data;
 var playing;
-
+var pausedAt;
+var paused;
+var startedAt;
 
 
 function initAudio(callback)
 {
-	try
-	{
-		// Fix up for prefixing
+	// try
+	// {
+	// 	// Fix up for prefixing
 		window.AudioContext = window.AudioContext || window.webkitAudioContext;
 		context = new AudioContext();
 
-		bufferLoader = new BufferLoader(context,
-		[
-		'music/test2.mp3'
-		],
-		finishedLoading
-		);
-		bufferLoader.load();
+		// bufferLoader = new BufferLoader(context,
+		// [
+		// 'music/test2.mp3'
+		// ],
+		// finishedLoading
+		// );
+		// bufferLoader.load();
 
 
 
 		processor = context.createScriptProcessor(2048, 1, 1);
-		analyzer = context.createAnalyser();
-		analyzer.fftSize = freq;
-		freq_data = new Uint8Array(analyzer.frequencyBinCount);
-
-		analyzer.connect(processor);
 		processor.connect(context.destination);
 
-	}
-	catch(e)
-	{
-		alert('Web Audio API is not supported in this browser');
-		console.log(e);
+		analyzer = context.createAnalyser();
+		analyzer.fftSize = freq;
+		
 
-	}
+		source = context.createBufferSource();
+		source.connect(analyzer);
+		analyzer.connect(processor);
+		source.connect(context.destination);
+		freq_data = new Uint8Array(analyzer.frequencyBinCount);
+		loadSound('music/queen.mp3');
+
+	//}
+	// catch(e)
+	// {
+	// 	alert('Web Audio API is not supported in this browser');
+	// 	console.log(e);
+
+	// }
 	console.log("Audio initialized.")
 	callback(true);
 }
 
-function finishedLoading(bufferList)
+// load the specified sound
+function loadSound(url) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.responseType = 'arraybuffer';
+    // When loaded decode the data
+    request.onload = function () {
+	    // decode the data
+        context.decodeAudioData(request.response, function (buff) {
+        // when the audio is decoded play the sound
+            
+            source.buffer = buff;
+
+            $("#play").attr("disabled", false);
+            console.log("Song loaded " + url + ".");
+        }, onError);
+    }
+    request.send();
+}
+
+ // log if an error occurs
+ function onError(e) {
+        console.log(e);
+ }
+
+function finishedLoading(bufferList, callback)
 {
 	source = context.createBufferSource();
 	source.buffer = bufferList[0];
@@ -65,7 +98,7 @@ function getCoefficients(gain, min ,max, l)
 	{
 		analyzer.getByteFrequencyData(freq_data);
 		var ratio = l / freq_data.length;
-		console.log(freq_data);
+		
 		trans_array = freq_data;
 		coeffs = new Float32Array(l);
 		var g;
@@ -101,14 +134,33 @@ function play()
 {
 	$("#play").attr("disabled", true);
 	$("#stop").attr("disabled", false);
-	source.start(0);
+	
+	document.getElementById("play").value="\u23F8";
+	
+	//source.start(0);
+	//source.loop = true;
+	paused = false;
 	playing = true;
+
+	if(pausedAt)
+	{
+		startedAt = Date.now() - pausedAt;
+		source.start(0, pausedAt / 1000);
+	}
+	else
+	{
+		startedAt = Date.now();
+		source.start(0);
+	}
 }
 
 function stop()
 {
 	$("#play").attr("disabled", false);
 	$("#stop").attr("disabled", true);
-	source.stop();
+	document.getElementById("play").value="\u25B6";
+	source.stop(0);
+	pausedAt = Date.now() - startedAt;
 	playing = false;
+	paused = true;
 }
